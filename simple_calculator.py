95348032d0b072e667003b3112b7125f868f9af3
Khaliq Ansari <khaliqansari771@gmail.com>
2018-12-28 09:23:48 -0800

    Add files via upload

---
+++ simple_calculator.py
@@ -0,0 +1,15 @@
+First_Number = int(input("Enter Your Number"))
+Second_Number = int(input("Enter Second Number"))
+
+Add = First_Number + Second_Number
+Sub = First_Number - Second_Number
+Mul = First_Number * Second_Number
+Divi = First_Number /Second_Number
+
+Remender = First_Number%Second_Number
+
+print(" Your Addition is ->" + str(Add))
+print(" Your Subtraction is ->" + str(Sub))
+print(" Your Multiplication is ->" + str(Mul))
+print(" Your Divition is ->" + str(Divi))
+print(" Your Remender is ->" + str(Remender))
